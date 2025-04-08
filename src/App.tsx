80d43ac4b523542464b8c61da5aba44d491dc03f
matze2006 <matthias.h06@gmail.com>
2025-04-08 08:44:06 +0200

    7 Setze Deine Footbal-Page mit React um planung (onenote)

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ function App() {
 
   return (
     <>
-      <div className="font-bold text-4xl ">
-        <h1>Bundesliga Übersicht</h1>
+      <div className="font-bold text-4xl">
+        <h1 className="justify-self-center">Bundesliga Übersicht</h1>
       </div>
     </>
   )
---
+++ src/components/SucheLiga.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function Suche({}: Props) {
+  return (
+    <div>Suche</div>
+  )
+}
